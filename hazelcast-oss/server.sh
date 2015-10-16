236c6afab0b96822539a890f3ec71c990b840b33
bilalyasar <bilalyasar45@gmail.com>
2015-10-16 16:45:54 +0300

    added start,stop script

---
+++ hazelcast-oss/server.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+PRG="$0"
+PRGDIR=`dirname "$PRG"`
+HAZELCAST_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`/hazelcast
+PID_FILE=$HAZELCAST_HOME/hazelcast_instance.pid
+
+
+#### you can enable following variables by uncommenting them
+
+#### minimum heap size
+# MIN_HEAP_SIZE=1G
+
+#### maximum heap size
+# MAX_HEAP_SIZE=1G
+
+
+if [ "x$MIN_HEAP_SIZE" != "x" ]; then
+	JAVA_OPTS="$JAVA_OPTS -Xms${MIN_HEAP_SIZE}"
+fi
+
+if [ "x$MAX_HEAP_SIZE" != "x" ]; then
+	JAVA_OPTS="$JAVA_OPTS -Xmx${MAX_HEAP_SIZE}"
+fi
+
+export CLASSPATH=$HAZELCAST_HOME/hazelcast-all-3.5.3.jar
+
+echo "########################################"
+echo "# RUN_JAVA=$RUN_JAVA"
+echo "# JAVA_OPTS=$JAVA_OPTS"
+echo "# starting now...."
+echo "########################################"
+
+echo "Process id for hazelcast instance is written to location: " $PID_FILE
+java -server $JAVA_OPTS com.hazelcast.core.server.StartServer &
+echo $! > ${PID_FILE}
+sleep infinity
