cbf0a96b9abcdc2bdce2a38a0bcfc61507d2bbdb
emre <emre@hazelcast.com>
2016-12-23 10:40:34 +0300

    master branch to have the latest stable images

---
+++ hazelcast-openshift/start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+PRG="$0"
+PRGDIR=`dirname "$PRG"`
+HAZELCAST_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`/hazelcast
+PID_FILE=$HAZELCAST_HOME/hazelcast_instance.pid
+
+if [ "x$MIN_HEAP_SIZE" != "x" ]; then
+	JAVA_OPTS="$JAVA_OPTS -Xms${MIN_HEAP_SIZE}"
+fi
+
+if [ "x$MAX_HEAP_SIZE" != "x" ]; then
+	JAVA_OPTS="$JAVA_OPTS -Xmx${MAX_HEAP_SIZE}"
+fi
+
+export CLASSPATH=$HAZELCAST_HOME/*:$CLASSPATH/*
+
+echo "########################################"
+echo "# RUN_JAVA=$RUN_JAVA"
+echo "# JAVA_OPTS=$JAVA_OPTS"
+echo "# starting now...."
+echo "########################################"
+
+java -server $JAVA_OPTS -Djava.net.preferIPv4Stack=true com.hazelcast.core.server.StartServer
