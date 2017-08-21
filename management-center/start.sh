f7e8ff7bcc0ceee8c4d3db529658b25aec11e706
Bilal Demirci <bilaldmrc@gmail.com>
2017-08-21 12:54:21 +0300

    classpath_arg added to the management-center image

--- management-center/start.sh
+++ management-center/start.sh
@@ -12,12 +12,10 @@ if [ "x$MAX_HEAP_SIZE" != "x" ]; then
 	JAVA_OPTS="$JAVA_OPTS -Xmx${MAX_HEAP_SIZE}"
 fi
 
-export CLASSPATH=$HAZELCAST_HOME/*:$CLASSPATH/*
-
 echo "########################################"
 echo "# RUN_JAVA=$RUN_JAVA"
 echo "# JAVA_OPTS=$JAVA_OPTS"
 echo "# starting now...."
 echo "########################################"
 
-java -server $JAVA_OPTS -Dhazelcast.mancenter.home=$MANCENTER_DATA -jar mancenter-$HZ_VERSION.war
+java -server $JAVA_OPTS -Dhazelcast.mancenter.home=$MANCENTER_DATA -cp mancenter-$HZ_VERSION.war:$HAZELCAST_HOME/*:$CLASSPATH_ARG/* Launcher 8080 mancenter
