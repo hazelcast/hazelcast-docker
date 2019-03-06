1e48c3dbec1a5dc57dc1ca24227c20e6165d93a0
Rafal Leszko <rafal@hazelcast.com>
2019-03-06 14:51:13 +0100

    Add JMX Prometheus agent to Hazelcast Enterprise

--- hazelcast-enterprise/start-hazelcast.sh
+++ hazelcast-enterprise/start-hazelcast.sh
@@ -31,6 +31,12 @@ else
   export JAVA_OPTS="${JAVA_OPTS} -Dhazelcast.mancenter.enabled=false"
 fi
 
+if [ -n "${PROMETHEUS_PORT}" ]; then
+  export JAVA_OPTS="${JAVA_OPTS} -javaagent:${HZ_HOME}/lib/jmx_prometheus_javaagent.jar=${PROMETHEUS_PORT}:${PROMETHEUS_CONFIG}"
+else
+  export JAVA_OPTS="${JAVA_OPTS}"
+fi
+
 if [ -n "${HZ_LICENSE_KEY}" ]; then
   export JAVA_OPTS="${JAVA_OPTS} -Dhazelcast.enterprise.license.key=${HZ_LICENSE_KEY}"
 else
