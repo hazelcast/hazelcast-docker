e4d9677f1bc2e5d4290821ba05c70aecdcbf5bf9
Muhammet Yazici <44066377+mtyazici@users.noreply.github.com>
2021-01-07 17:51:27 +0300

    Update hazelcast-oss/get-hz-all-url.sh
    Co-authored-by: Rafał Leszko <rafal@hazelcast.com>

--- hazelcast-oss/get-hz-all-url.sh
+++ hazelcast-oss/get-hz-all-url.sh
@@ -3,7 +3,7 @@
 if [[ "${HZ_VERSION}" == *"SNAPSHOT"* ]]
 then
     xml=$(curl -sSL https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-all/${HZ_VERSION}/maven-metadata.xml)
-    version=$(echo $xml | xpath -q  -e  '/metadata/versioning/snapshotVersions/snapshotVersion[1]/value' | grep -oP '(?<=<value>).*(?=</value>)')
+    version=$(echo $xml | xpath -q -e '/metadata/versioning/snapshotVersions/snapshotVersion[1]/value' | grep -oP '(?<=<value>).*(?=</value>)')
     url="https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-all/${HZ_VERSION}/hazelcast-all-${version}.jar"
 else
     url="https://repo1.maven.org/maven2/com/hazelcast/hazelcast-all/${HZ_VERSION}/hazelcast-all-${HZ_VERSION}.jar"
