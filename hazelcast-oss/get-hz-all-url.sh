107b67909a12e3f48411f91afaa376b98c52f78c
Muhammet Yazici <muhammet.yazici@boun.edu.tr>
2021-01-07 18:11:51 +0300

    Add suggestions from code review

--- hazelcast-oss/get-hz-all-url.sh
+++ hazelcast-oss/get-hz-all-url.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This is a simple script imitating what maven does for snapshot versions. We are not using maven because currently Docker Buildx and QEMU on Github Actions
+# don't work with Java on architectures ppc64le and s390x.
+# If the version is snapshot, the script downloads the 'maven-metadata.xml' and parses it for the latest snapshot version. 'maven-metadata.xml' only holds the values for 
+# the latest snapshot version. Thus, the [1] in snapshotVersion[1] is arbitrary because all of elements in the list have same value. The list consists of 'jar', 'pom', 'sources' and 'javadoc'.
 if [[ "${HZ_VERSION}" == *"SNAPSHOT"* ]]
 then
     xml=$(curl -sSL https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-all/${HZ_VERSION}/maven-metadata.xml)
