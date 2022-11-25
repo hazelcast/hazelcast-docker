f32021b601baabbfb4f1c51e2f6cc10e7e0e5ea5
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2022-11-25 16:28:28 +0100

    Add retries to RHEL image publishing

--- .github/scripts/publish-rhel.sh
+++ .github/scripts/publish-rhel.sh
@@ -90,6 +90,7 @@ publish_the_image()
     echo "Publishing the image $IMAGE_ID..."
     RESPONSE=$( \
         curl --silent \
+            --retry 5 --retry-all-errors \
             --request POST \
             --header "X-API-KEY: ${RHEL_API_KEY}" \
             --header 'Cache-Control: no-cache' \
