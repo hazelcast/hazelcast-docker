a14399c743aa04af85e84c3972739b60d9de5209
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2022-11-30 19:59:03 +0100

    Add simple smoke test for OSS Dockerfile (#522) [5.0.z] (#529)
    * Add simple smoke test for OSS Dockerfile
    
    Co-authored-by: František Hartman <frant.hartm@gmail.com>
    (cherry picked from commit 7bb30f30217cdea21949155f2298869f6671549f)

---
+++ .github/scripts/simple-smoke-test.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+export CLC_VERSION=v5.2.0-beta3-preview1
+
+function test_docker_image() {
+    local image=$1
+    local container_name=$2
+    echo "Starting container '$container_name' from image '$image'"
+    docker run -it --name "$container_name" -d -p5701:5701 "$image"
+    local key="some-key"
+    local expected="some-value"
+    echo "Putting value '$expected' for key '$key'"
+    clc map put -n some-map $key $expected
+    echo "Getting value for key '$key'"
+    local actual
+    actual=$(clc map get -n some-map $key)
+    echo "Stopping container $container_name}"
+    docker stop "$container_name"
+
+    if [ "$expected" != "$actual" ]; then
+        echo "Expected to read '${expected}' but got '${actual}'"
+        exit 1;
+    fi
+}
+
+function install_clc() {
+  CLC_URL="https://github.com/hazelcast/hazelcast-commandline-client/releases/download/${CLC_VERSION}/hazelcast-clc_${CLC_VERSION}_linux_amd64.tar.gz"
+  curl -L $CLC_URL | tar xzf - --strip-components=1 -C /usr/local/bin
+  chmod +x /usr/local/bin/clc
+}
+
+install_clc
+test_docker_image "$@"
