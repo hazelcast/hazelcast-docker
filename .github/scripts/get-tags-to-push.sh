938969d0570ec6c4e11f0feeb84bd1ca62afb0d9
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2023-04-16 19:41:39 +0200

    Add tests for get_tags_to_push

--- .github/scripts/get-tags-to-push.sh
+++ .github/scripts/get-tags-to-push.sh
@@ -10,29 +10,31 @@ function get_latest_version() {
 }
 
 function verlte() {
-    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+  [ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
 }
 
-VERSION_TO_RELEASE=$1
-MINOR_VERSION_TO_RELEASE=${VERSION_TO_RELEASE%.*}
-MAJOR_VERSION_TO_RELEASE=${MINOR_VERSION_TO_RELEASE%.*}
+function get_tags_to_push() {
+  VERSION_TO_RELEASE=$1
+  MINOR_VERSION_TO_RELEASE=${VERSION_TO_RELEASE%.*}
+  MAJOR_VERSION_TO_RELEASE=${MINOR_VERSION_TO_RELEASE%.*}
 
-LATEST_FOR_MINOR=$(find_last_matching_version $MINOR_VERSION_TO_RELEASE)
-LATEST_FOR_MAJOR=$(find_last_matching_version $MAJOR_VERSION_TO_RELEASE)
-LATEST=$(get_latest_version)
+  LATEST_FOR_MINOR=$(find_last_matching_version $MINOR_VERSION_TO_RELEASE)
+  LATEST_FOR_MAJOR=$(find_last_matching_version $MAJOR_VERSION_TO_RELEASE)
+  LATEST=$(get_latest_version)
 
-TAGS_TO_PUSH+=($VERSION_TO_RELEASE)
+  TAGS_TO_PUSH+=($VERSION_TO_RELEASE)
 
-if verlte "$LATEST_FOR_MINOR" "$VERSION_TO_RELEASE"; then
-   TAGS_TO_PUSH+=($MINOR_VERSION_TO_RELEASE)
-fi
+  if verlte "$LATEST_FOR_MINOR" "$VERSION_TO_RELEASE"; then
+    TAGS_TO_PUSH+=($MINOR_VERSION_TO_RELEASE)
+  fi
 
-if verlte "$LATEST_FOR_MAJOR" "$VERSION_TO_RELEASE"; then
-   TAGS_TO_PUSH+=($MAJOR_VERSION_TO_RELEASE)
-fi
+  if verlte "$LATEST_FOR_MAJOR" "$VERSION_TO_RELEASE"; then
+    TAGS_TO_PUSH+=($MAJOR_VERSION_TO_RELEASE)
+  fi
 
-if verlte "$LATEST" "$VERSION_TO_RELEASE"; then
-   TAGS_TO_PUSH+=(latest)
-fi
+  if verlte "$LATEST" "$VERSION_TO_RELEASE"; then
+    TAGS_TO_PUSH+=(latest)
+  fi
 
-echo "${TAGS_TO_PUSH[@]}"
+  echo "${TAGS_TO_PUSH[@]}"
+}
---
+++ .github/scripts/get-tags-to-push_tests.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+function findScriptDir() {
+  CURRENT=$PWD
+
+  DIR=$(dirname "$0")
+  cd "$DIR" || exit
+  TARGET_FILE=$(basename "$0")
+
+  # Iterate down a (possible) chain of symlinks
+  while [ -L "$TARGET_FILE" ]
+  do
+      TARGET_FILE=$(readlink "$TARGET_FILE")
+      DIR=$(dirname "$TARGET_FILE")
+      cd "$DIR" || exit
+      TARGET_FILE=$(basename "$TARGET_FILE")
+  done
+
+  SCRIPT_DIR=$(pwd -P)
+  # Restore current directory
+  cd "$CURRENT" || exit
+}
+
+findScriptDir
+
+. "$SCRIPT_DIR"/assert.sh/assert.sh
+. "$SCRIPT_DIR"/get-tags-to-push.sh
+
+TESTS_RESULT=0
+
+function assert_tags_to_push {
+  local VERSION_TO_RELEASE=$1
+  local EXPECTED_TAGS_TO_PUSH=$2
+  local ACTUAL_TAGS_TO_PUSH=$(get_tags_to_push "$VERSION_TO_RELEASE")
+  assert_eq "$EXPECTED_TAGS_TO_PUSH" "$ACTUAL_TAGS_TO_PUSH" "Tags to push for version $VERSION_TO_RELEASE should be equal to $EXPECTED_TAGS_TO_PUSH " || TESTS_RESULT=$?
+}
+
+log_header "Tests for get_tags_to_push"
+assert_tags_to_push "5.2.0" "5.2.0"
+assert_tags_to_push "5.2.1" "5.2.1"
+assert_tags_to_push "5.1.99" "5.1.99 5.1"
+assert_tags_to_push "5.99.0" "5.99.0 5.99 5 latest" #this needs adjustment when we release 6.0
+assert_tags_to_push "99.0.0" "99.0.0 99.0 99 latest"
+
+assert_eq 0 "$TESTS_RESULT" "All tests should pass"
