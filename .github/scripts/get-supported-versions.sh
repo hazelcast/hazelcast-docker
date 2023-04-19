98a67ee39fd7bb532faa0340f169e573f67ebefd
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2023-04-19 11:12:16 +0200

    Rebuild only supported latest patch versions

--- .github/scripts/get-supported-versions.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-function get_supported_versions() {
-    local MINIMAL_VERSION=$1
-    VERSIONS=$(git tag | grep '^v' | cut -c2- | sed -n "/^${MINIMAL_VERSION}\$/,\$p" | grep -v BETA)
-    echo "$VERSIONS"
-}
