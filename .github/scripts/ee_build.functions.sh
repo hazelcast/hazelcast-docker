a68f17540640a229f0235f433e3a51a2c31a3390
Jack <jack.green@hazelcast.com>
2024-05-08 10:02:54 +0100

    Avoid temporary files

--- .github/scripts/ee_build.functions.sh
+++ .github/scripts/ee_build.functions.sh
@@ -15,16 +15,14 @@ function get_hz_dist_zip() {
   if [[ -n "${hz_variant}" ]]; then suffix="-${hz_variant}"; fi
 
   if [[ "${hz_version}" == *"SNAPSHOT"* ]]
-  then
-      curl -O -fsSL https://repository.hazelcast.com/snapshot/com/hazelcast/hazelcast-enterprise-distribution/"${hz_version}"/maven-metadata.xml
+  then  
       classifier_filter="not(classifier)"
       if [ -n "$hz_variant" ]; then
           classifier_filter="classifier='$hz_variant'"
       fi
-      version=$(xmllint --xpath "/metadata/versioning/snapshotVersions/snapshotVersion[extension='zip' and $classifier_filter]/value/text()" maven-metadata.xml)
+      version=$(curl -fsSL https://repository.hazelcast.com/snapshot/com/hazelcast/hazelcast-enterprise-distribution/"${hz_version}"/maven-metadata.xml | xmllint --xpath "/metadata/versioning/snapshotVersions/snapshotVersion[extension='zip' and $classifier_filter]/value/text()" -)
 
       url="https://repository.hazelcast.com/snapshot/com/hazelcast/hazelcast-enterprise-distribution/${hz_version}/hazelcast-enterprise-distribution-${version}${suffix}.zip"
-      rm maven-metadata.xml
   else
       url="https://repository.hazelcast.com/release/com/hazelcast/hazelcast-enterprise-distribution/${hz_version}/hazelcast-enterprise-distribution-${hz_version}${suffix}.zip"
   fi
