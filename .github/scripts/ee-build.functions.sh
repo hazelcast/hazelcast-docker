5f481568e13bbdc0eb80558a37e43746d2f28536
Jack Green <JackPGreen@Gmail.com>
2024-05-14 17:03:43 +0100

    Add `maven-metadata.xml` example

--- .github/scripts/ee-build.functions.sh
+++ .github/scripts/ee-build.functions.sh
@@ -6,6 +6,8 @@ set -euo pipefail
 # don't work with Java on architectures ppc64le and s390x. When the problem is fixed we will revert back to using maven.
 # If the version is snapshot, the script downloads the 'maven-metadata.xml' and parses it for the snapshot version. 'maven-metadata.xml' only holds the values for
 # the latest snapshot version. Thus, the [1] in snapshotVersion[1] is arbitrary because all of elements in the list have same value. The list consists of 'jar', 'pom', 'sources' and 'javadoc'.
+#
+# 'maven-metadata.xml' example - https://repo1.maven.org/maven2/com/hazelcast/hazelcast/maven-metadata.xml
 function get_hz_dist_zip() {
   local hz_variant=$1
   local hz_version=$2
