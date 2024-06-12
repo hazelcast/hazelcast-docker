93f73b6bd4630c9c8e280d39e93eb4786a3c9cc1
Jack <jack.green@hazelcast.com>
2024-06-12 09:27:50 +0100

    Adjust quoting

--- .github/scripts/oss-build.functions.sh
+++ .github/scripts/oss-build.functions.sh
@@ -14,7 +14,7 @@ function get_hz_dist_zip() {
   then
       # DI-95 - Do not rely on the OSS distribution zip in the hazelcast-docker PR builder
       # https://hazelcast.atlassian.net/browse/DI-95
-      url="$(aws s3 presign s3://hazelcast/distribution-snapshot/hazelcast-"${hz_version}""${suffix}".zip --expires-in 600)"
+      url="$(aws s3 presign "s3://hazelcast/distribution-snapshot/hazelcast-${hz_version}${suffix}.zip" --expires-in 600)"
   else
       url="https://repo1.maven.org/maven2/com/hazelcast/hazelcast-distribution/${hz_version}/hazelcast-distribution-${hz_version}${suffix}.zip"
   fi
