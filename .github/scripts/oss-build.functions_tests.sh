e316d2c8a750ed9a242a5cf9fd462bac6898bec8
Jack Green <jack.green@hazelcast.com>
2024-06-15 22:01:09 +0100

    Remove `suffix` parameter from scripts (#754)
    `suffix` is a hardcoded derivative of `variant`, and can instead be
    derived dynamically.
    
    `tag_image_push_rhel.yml` used `suffix`, but never actually set it
    (unlike `tag_image_push.yml` which did), so hardcoded it to `""`
    instead.
    
    Fixes: https://github.com/hazelcast/hazelcast-docker/issues/753

--- .github/scripts/oss-build.functions_tests.sh
+++ .github/scripts/oss-build.functions_tests.sh
@@ -32,14 +32,13 @@ TESTS_RESULT=0
 function assert_get_hz_dist_zip {
   local hz_variant=$1
   local hz_version=$2
-  local suffix=$3
-  local expected_url=$4
-  local actual_url=$(get_hz_dist_zip "$hz_variant" "$hz_version" "$suffix")
-  assert_eq "$expected_url" "$actual_url" "Expected URL for variant \"$hz_variant\", version \"$hz_version\", suffix \"$suffix\"" || TESTS_RESULT=$?
+  local expected_url=$3
+  local actual_url=$(get_hz_dist_zip "$hz_variant" "$hz_version")
+  assert_eq "$expected_url" "$actual_url" "Expected URL for variant \"$hz_variant\", version \"$hz_version\"" || TESTS_RESULT=$?
 }
 
 log_header "Tests for get_hz_dist_zip"
-assert_get_hz_dist_zip slim 5.4.0 -slim https://repo1.maven.org/maven2/com/hazelcast/hazelcast-distribution/5.4.0/hazelcast-distribution-5.4.0-slim.zip
-assert_get_hz_dist_zip "" 5.4.0 "" https://repo1.maven.org/maven2/com/hazelcast/hazelcast-distribution/5.4.0/hazelcast-distribution-5.4.0.zip
+assert_get_hz_dist_zip slim 5.4.0 https://repo1.maven.org/maven2/com/hazelcast/hazelcast-distribution/5.4.0/hazelcast-distribution-5.4.0-slim.zip
+assert_get_hz_dist_zip "" 5.4.0 https://repo1.maven.org/maven2/com/hazelcast/hazelcast-distribution/5.4.0/hazelcast-distribution-5.4.0.zip
 
 assert_eq 0 "$TESTS_RESULT" "All tests should pass"
