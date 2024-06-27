6351554ab333541e176f8c2d4ede29332209d566
Jack <jack.green@hazelcast.com>
2024-06-27 16:47:19 +0100

    Reduce duplication in scripts between test and released images - HAZELCAST_ZIP_URL [DI-156] [5.4.z]
    Backport of https://github.com/hazelcast/hazelcast-docker/pull/766
    
    To simplify conflict resolution, also backports:
    - https://github.com/hazelcast/hazelcast-docker/pull/754
    - https://github.com/hazelcast/hazelcast-docker/pull/756

--- .github/scripts/simple-smoke-test.sh
+++ .github/scripts/simple-smoke-test.sh
@@ -6,6 +6,27 @@ set -o pipefail
 function test_docker_image() {
     local image=$1
     local container_name=$2
+    local expected_distribution_type=$3
+
+    if [ "$(docker ps --all --quiet --filter name="$container_name")" ]; then
+      echo "Removing existing '$container_name' container"
+      docker container rm --force "$container_name"
+    fi
+
+    echo "Checking if $image is EE"
+    if docker run --rm $image bash -c 'compgen -G lib/*enterprise*'; then
+      echo "EE contents identified"
+      distribution_type="ee"
+    else
+      echo "No EE contents identified - assuming OSS"
+      distribution_type="oss"
+    fi
+
+    if [[ "$distribution_type" != "$expected_distribution_type" ]]; then
+      echo "Distribution was $distribution_type, not $expected_distribution_type as expected"
+      exit 1
+    fi
+
     echo "Starting container '$container_name' from image '$image'"
     docker run -it --name "$container_name" -e HZ_LICENSEKEY -e HZ_INSTANCETRACKING_FILENAME -d -p5701:5701 "$image"
     local key="some-key"
