1b9eb6a4a7f4ee24b579c7048f8b1e52dd32db40
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2024-06-28 14:37:27 +0200

    Trigger README rebuild only

--- .github/scripts/build.functions.sh
+++ .github/scripts/build.functions.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 function should_build_oss() {
 
   local release_type=$1
-  if [[ $release_type == "ALL" || $release_type == "OSS" ]]; then
+  if [[ $release_type =~ ^(ALL|OSS)$ ]]; then
     echo "yes"
   else
     echo "no"
@@ -19,7 +19,31 @@ function should_build_oss() {
 function should_build_ee() {
 
   local release_type=$1
-  if [[ $release_type == "ALL" || $release_type == "EE" ]]; then
+  if [[ $release_type =~ ^(ALL|EE)$ ]]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+# Checks if we should rebuild the docker EE image description.
+# Returns "yes" if we should rebuild it or "no" if we shouldn't.
+function should_build_readme_ee() {
+
+  local release_type=$1
+  if [[ $release_type =~ ^(ALL|EE|README)$ ]]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+# Checks if we should rebuild the docker OSS image description.
+# Returns "yes" if we should rebuild it or "no" if we shouldn't.
+function should_build_readme_oss() {
+
+  local release_type=$1
+  if [[ $release_type =~ ^(ALL|OSS|README)$ ]]; then
     echo "yes"
   else
     echo "no"
