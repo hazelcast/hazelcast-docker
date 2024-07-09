66ac3af6b656ff73bf6067a42d802477929e74b5
Jack Green <jack.green@hazelcast.com>
2024-07-09 09:16:26 +0100

    Update `simple-smoke-test` to check Hazelcast version [DI-163] [5.2.z]
    - adds a version check
    - refactors `simple-smoke-test`
       - adds functions to aid readability now it's got a bit larger
       - resolve Spellcheck IDE warnings
    
    Fixes: [DI-163](https://hazelcast.atlassian.net/browse/DI-163)
    
    [DI-163]:
    https://hazelcast.atlassian.net/browse/DI-163?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Co-authored-by: Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
    (cherry picked from commit 64787141b681f60d852155f04fd0809abdc8609d)

--- .github/scripts/simple-smoke-test.sh
+++ .github/scripts/simple-smoke-test.sh
@@ -1,20 +1,39 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
+set -o errexit -o nounset -o pipefail
 
 function test_docker_image() {
     local image=$1
     local container_name=$2
     local expected_distribution_type=$3
+    local expected_version=$4
 
-    if [ "$(docker ps --all --quiet --filter name="$container_name")" ]; then
-      echo "Removing existing '$container_name' container"
-      docker container rm --force "$container_name"
+    remove_container_if_exists "${container_name}"
+
+    check_distribution_type "${image}" "${expected_distribution_type}"
+    check_image_hz_version "${image}" "${expected_version}"
+
+    test_map_read_write "${image}" "${container_name}"
+}
+
+function remove_container_if_exists() {
+    local container_name=$1
+
+    local containers
+    containers=$(docker ps --all --quiet --filter name="${container_name}")
+
+    if [[ -n "${containers}" ]]; then
+      echo "Removing existing '${container_name}' container"
+      docker container rm --force "${container_name}"
     fi
+}
 
-    echo "Checking if $image is EE"
-    if docker run --rm $image bash -c 'compgen -G lib/*enterprise*'; then
+function check_distribution_type() {
+    local image=$1
+    local expected_distribution_type=$2
+
+    echo "Checking ${image} distribution type"
+    if docker run --rm "${image}" bash -c 'compgen -G lib/*enterprise*'; then
       echo "EE contents identified"
       distribution_type="ee"
     else
@@ -22,29 +41,49 @@ function test_docker_image() {
       distribution_type="oss"
     fi
 
-    if [[ "$distribution_type" != "$expected_distribution_type" ]]; then
-      echo "Distribution was $distribution_type, not $expected_distribution_type as expected"
+    if [[ "${distribution_type}" != "${expected_distribution_type}" ]]; then
+      echoerr "Image ${image} should contain ${expected_distribution_type} distribution but ${distribution_type} was detected"
       exit 1
     fi
+}
 
-    echo "Starting container '$container_name' from image '$image'"
-    docker run -it --name "$container_name" -e HZ_LICENSEKEY -e HZ_INSTANCETRACKING_FILENAME -d -p5701:5701 "$image"
+function check_image_hz_version() {
+    local image=$1
+    local expected_version=$2
+
+    echo "Checking ${image} version"
+    local version
+    version=$(docker run --rm "${image}" bin/hz-cli --version | awk '/Hazelcast/ {print $2}')
+    if [[ "${version}" == "${expected_version}" ]]; then
+      echo "${image} version identified as ${version}"
+    else
+      echoerr "${image} version was ${version}, not ${expected_version} as expected"
+      exit 1
+    fi
+}
+
+function test_map_read_write() {
+    local image=$1
+    local expected_distribution_type=$2
+
+    echo "Starting container '${container_name}' from image '${image}'"
+    docker run -it --name "${container_name}" -e HZ_LICENSEKEY -e HZ_INSTANCETRACKING_FILENAME -d -p5701:5701 "${image}"
     local key="some-key"
     local expected="some-value"
-    echo "Putting value '$expected' for key '$key'"
-    while ! clc --timeout 5s map set -n some-map $key $expected --log.path stderr
+    echo "Putting value '${expected}' for key '${key}'"
+    while ! clc --timeout 5s map set -n some-map "${key}" "${expected}" --log.path stderr
     do
       echo "Retrying..."
       sleep 3
     done
-    echo "Getting value for key '$key'"
+    echo "Getting value for key '${key}'"
     local actual
-    actual=$(clc map get --format delimited -n some-map $key --log.path stderr)
-    echo "Stopping container $container_name}"
-    docker stop "$container_name"
+    actual=$(clc map get --format delimited -n some-map "${key}" --log.path stderr)
+    echo "Stopping container ${container_name}"
+    docker stop "${container_name}"
 
-    if [ "$expected" != "$actual" ]; then
-        echo "Expected to read '${expected}' but got '${actual}'"
+    if [[ "${expected}" != "${actual}" ]]; then
+        echoerr "Expected to read '${expected}' but got '${actual}'"
         exit 1;
     fi
 }
@@ -55,9 +94,15 @@ function install_clc() {
       echo "Retrying clc installation..."
       sleep 3
     done
-  export PATH=$PATH:$HOME/.hazelcast/bin
+  export PATH=${PATH}:${HOME}/.hazelcast/bin
   clc config add default cluster.name=dev cluster.address=localhost
 }
+
+# Prints the given message to stderr
+function echoerr() {
+  echo "ERROR - $*" 1>&2;
+}
+
 
 install_clc
 test_docker_image "$@"
