e676ec1f3a41b47de0fcd9a4f8b9503d1fa911bf
Jack <jack.green@hazelcast.com>
2024-08-01 12:01:19 +0100

    Move `derive_expected_distribution_type` to function

--- .github/scripts/simple-smoke-test.sh
+++ .github/scripts/simple-smoke-test.sh
@@ -29,27 +29,33 @@ function stop_container() {
     docker stop "${container_name}"
 }
 
+function derive_expected_distribution_type() {
+  local input_distribution_type=$1
+
+  case "${input_distribution_type}" in
+    "oss")
+      echo "Hazelcast Platform"
+      ;;
+    "ee")
+      echo "Hazelcast Enterprise"
+      ;;
+    *)
+      echoerr "Unrecognized distribution type ${input_distribution_type}"
+      exit 1
+      ;;
+  esac
+}
+
 image=$1
 container_name=$2
 input_distribution_type=$3
 expected_version=$4
 
-case "${input_distribution_type}" in
-  "oss")
-    expected_distribution_type="Hazelcast Platform"
-    ;;
-  "ee")
-    expected_distribution_type="Hazelcast Enterprise"
-    ;;
-  *)
-    echoerr "Unrecognized distribution type ${input_distribution_type}"
-    exit 1
-    ;;
-esac
 
 remove_container_if_exists
 start_container
 
 trap stop_container EXIT
 
+expected_distribution_type=$(derive_expected_distribution_type "${input_distribution_type}")
 test_package "${expected_distribution_type}" "${expected_version}"
