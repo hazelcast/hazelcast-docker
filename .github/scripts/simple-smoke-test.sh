a4826affa357dfbc1e27caa8b561126a4486f807
Jack Green <jack.green@hazelcast.com>
2024-08-01 12:13:57 +0100

    Split `simple-smoke-test.sh` to be used elsewhere [DI-215] (#790)
    `simple-smoke-test.sh` is a useful suite of tests to validate a packaged
    Hazelcast instance - but is Docker-centric.
    
    - Refactored to split the script into two halves:
    - `abstract-simple-smoke-test` that tests against an abstract Hazelcast
    instance
      - `simple-smoke-test` a Docker-specific implementation
    - Refactored the metadata checks to search the instance's logs for the
    variant and version (i.e. OSS/EE, `5.5.0` etc) rather than inspecting
    the containers' filesystem
    - an existing test elsewhere in the codebase already does this so we
    already have some dependency on keeping the output consistent, although
    I can't currently find the test in question
    - Added extra coverage from [`hazelcast-packaging`'s
    `check-hazelcast-health.sh`](https://github.com/hazelcast/hazelcast-packaging/blob/bd417d0604b57ae2f72234d8781929fdf86b0687/check-hazelcast-health.sh)
    
    Required for [DI-215](https://hazelcast.atlassian.net/browse/DI-215)
    
    [DI-215]:
    https://hazelcast.atlassian.net/browse/DI-215?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- .github/scripts/simple-smoke-test.sh
+++ .github/scripts/simple-smoke-test.sh
@@ -1,24 +1,11 @@
 #!/usr/bin/env bash
 
-set -o errexit -o nounset -o pipefail
+set -o errexit
 
-function test_docker_image() {
-    local image=$1
-    local container_name=$2
-    local expected_distribution_type=$3
-    local expected_version=$4
-
-    remove_container_if_exists "${container_name}"
-
-    check_distribution_type "${image}" "${expected_distribution_type}"
-    check_image_hz_version "${image}" "${expected_version}"
-
-    test_map_read_write "${image}" "${container_name}"
-}
+# shellcheck source=../.github/scripts/abstract-simple-smoke-test.sh
+. .github/scripts/abstract-simple-smoke-test.sh
 
 function remove_container_if_exists() {
-    local container_name=$1
-
     local containers
     containers=$(docker ps --all --quiet --filter name="${container_name}")
 
@@ -28,77 +15,18 @@ function remove_container_if_exists() {
     fi
 }
 
-function check_distribution_type() {
-    local image=$1
-    local expected_distribution_type=$2
-
-    echo "Checking ${image} distribution type"
-    if docker run --rm "${image}" bash -c 'compgen -G lib/*enterprise*'; then
-      echo "EE contents identified"
-      distribution_type="ee"
-    else
-      echo "No EE contents identified - assuming OSS"
-      distribution_type="oss"
-    fi
-
-    if [[ "${distribution_type}" != "${expected_distribution_type}" ]]; then
-      echoerr "Image ${image} should contain ${expected_distribution_type} distribution but ${distribution_type} was detected"
-      exit 1
-    fi
-}
-
-function check_image_hz_version() {
-    local image=$1
-    local expected_version=$2
-
-    echo "Checking ${image} version"
-    local version
-    version=$(docker run --rm "${image}" bin/hz-cli --version | awk '/Hazelcast/ {print $2}')
-    if [[ "${version}" == "${expected_version}" ]]; then
-      echo "${image} version identified as ${version}"
-    else
-      echoerr "${image} version was ${version}, not ${expected_version} as expected"
-      exit 1
-    fi
-}
-
-function test_map_read_write() {
-    local image=$1
-    local expected_distribution_type=$2
-
+function start_container() {
     echo "Starting container '${container_name}' from image '${image}'"
     docker run -it --name "${container_name}" -e HZ_LICENSEKEY -e HZ_INSTANCETRACKING_FILENAME -d -p5701:5701 "${image}"
-    local key="some-key"
-    local expected="some-value"
-    echo "Putting value '${expected}' for key '${key}'"
-    while ! clc --timeout 5s map set -n some-map "${key}" "${expected}" --log.path stderr
-    do
-      echo "Retrying..."
-      sleep 3
-    done
-    echo "Getting value for key '${key}'"
-    local actual
-    actual=$(clc map get --format delimited -n some-map "${key}" --log.path stderr)
-    echo "Stopping container ${container_name}"
-    docker stop "${container_name}"
-
-    if [[ "${expected}" != "${actual}" ]]; then
-        echoerr "Expected to read '${expected}' but got '${actual}'"
-        exit 1;
-    fi
 }
 
-function check_java_version() {
-    local expected_major_version=$1
-    local actual_major_version
-    actual_major_version=$(docker run --rm "${image}" sh -c 'java -version 2>&1 | head -n 1 | awk -F "\"" "{print \$2}" | awk -F "." "{print \$1}"')
+function get_hz_logs() {
+    docker logs "${container_name}"
+}
 
-    if [[ "${expected_major_version}" == "${actual_major_version}" ]]; then
-      echo "Expected Java version (${expected_distribution_type}) identified."
-    else
-      echoerr "Expected Java version '${expected_major_version}' but got '${actual_major_version}'"
-      exit 1;
-    fi
+function stop_container() {
+    echo "Stopping container ${container_name}"
+    docker stop "${container_name}"
 }
 
 function derive_expected_distribution_type() {
@@ -122,7 +50,6 @@ image=$1
 container_name=$2
 input_distribution_type=$3
 expected_version=$4
-expected_java_major_version=$5
 
 
 remove_container_if_exists
@@ -132,4 +59,3 @@ trap stop_container EXIT
 
 expected_distribution_type=$(derive_expected_distribution_type "${input_distribution_type}")
 test_package "${expected_distribution_type}" "${expected_version}"
-check_java_version "${expected_java_major_version}"
