47045ec5895a4b1750f96acb7cd2703eca48f071
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2024-08-01 14:38:48 +0200

    Add missing scripts [DI-210][5.2.z] (#792)
    Add missing script and refactorings that were missed in
    https://github.com/hazelcast/hazelcast-docker/pull/791

--- .github/scripts/oss-build.functions_tests.sh
+++ .github/scripts/oss-build.functions_tests.sh
@@ -38,7 +38,10 @@ function assert_get_hz_dist_zip {
 }
 
 log_header "Tests for get_hz_dist_zip"
+export HZ_SNAPSHOT_INTERNAL_USERNAME=dummy_user
+export HZ_SNAPSHOT_INTERNAL_PASSWORD=dummy_password
 assert_get_hz_dist_zip slim 5.4.0 https://repo1.maven.org/maven2/com/hazelcast/hazelcast-distribution/5.4.0/hazelcast-distribution-5.4.0-slim.zip
 assert_get_hz_dist_zip "" 5.4.0 https://repo1.maven.org/maven2/com/hazelcast/hazelcast-distribution/5.4.0/hazelcast-distribution-5.4.0.zip
+assert_get_hz_dist_zip "" 5.3.9-SNAPSHOT https://dummy_user:dummy_password@repository.hazelcast.com/snapshot-internal/com/hazelcast/hazelcast-distribution/5.3.9-SNAPSHOT/hazelcast-distribution-5.3.9-SNAPSHOT.zip
 
 assert_eq 0 "$TESTS_RESULT" "All tests should pass"
