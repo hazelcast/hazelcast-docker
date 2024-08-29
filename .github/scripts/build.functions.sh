c0c0af5ecd415a7521b3551ef34acd08987c6002
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2024-08-29 10:33:08 +0200

    Add option to rebuild image description without rebuilding images [DI-164] (#793)
    Currently we have to rebuild the OS or EE image to rebuild the docker
    hub description
    
    This PR adds option to rebuild image descriptions without rebuilding
    images [DI-164]
    
    
    [DI-164]:
    https://hazelcast.atlassian.net/browse/DI-164?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- .github/scripts/build.functions.sh
+++ .github/scripts/build.functions.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 function should_build_oss() {
 
   local release_type=$1
-  if [[ $release_type == "ALL" || $release_type == "OSS" ]]; then
+  if [[ $release_type =~ ^(ALL|OSS)$ ]]; then
     echo "yes"
   else
     echo "no"
@@ -15,11 +15,32 @@ function should_build_oss() {
 }
 
 # Checks if we should build the OSS docker image.
-# Returns "yes" if we should build it or "no" if we shouldn't.
 function should_build_ee() {
 
   local release_type=$1
-  if [[ $release_type == "ALL" || $release_type == "EE" ]]; then
+  if [[ $release_type =~ ^(ALL|EE)$ ]]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+# Checks if we should rebuild the docker EE image description.
+function should_build_readme_ee() {
+
+  local release_type=$1
+  if [[ $release_type =~ ^(ALL|EE|README)$ ]]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+# Checks if we should rebuild the docker OSS image description.
+function should_build_readme_oss() {
+
+  local release_type=$1
+  if [[ $release_type =~ ^(ALL|OSS|README)$ ]]; then
     echo "yes"
   else
     echo "no"
