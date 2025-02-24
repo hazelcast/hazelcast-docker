9c3dfb4f85594ba9355c4ec4a160cadffa199662
Jack Green <jack.green@hazelcast.com>
2025-02-24 16:43:07 +0000

    Ensure RHEL tests use a valid version of Management Center [DI-422] (#880)
    The RHEL publish tests the build by connecting an Management Center
    instance to it.
    
    In https://github.com/hazelcast/hazelcast-docker/pull/847 this was
    changed to use a _matching_ version of Hazelcast & Management Center -
    which isn't correct in all cases (e.g. `5.5.2` Hazelcast & `5.6.0`
    Management Center)
    
    Before this change, it used the latest version of Management Center -
    derived by inspecting the tags of the (archived)
    https://github.com/hazelcast/management-center-openshift repo - but the
    last tag here was `5.3.2` so it wasn't really the latest at all.
    
    In [consultation with the Management Center
    team](https://hazelcast.slack.com/archives/C9S7BV74Z/p1738850975557879),
    latest is the best option.
    
    Changes:
    - rolls back to something _inspired_ by the previous implementation -
    choses latest based on tags
    - tags are scraped from the `hazelcast/management-center` repo
    - tags are queried using the `gh` cli tool to avoid checking out the
    whole repo
    
    I don't think we should backport this change, as while it's not correct
    in earlier versions, it doesn't cause any issues.
    
    Fixes: [DI-422](https://hazelcast.atlassian.net/browse/DI-422)
    
    [DI-422]:
    https://hazelcast.atlassian.net/browse/DI-422?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- .github/scripts/version.functions_tests.sh
+++ .github/scripts/version.functions_tests.sh
@@ -64,6 +64,13 @@ function assert_latest_patch_versions_not_contain {
   assert_not_contain "$ACTUAL_LATEST_PATCH_VERSIONS" "$EXPECTED_VERSION" "Latest patch versions starting from $MINIMAL_SUPPORTED_VERSION should NOT contain $EXPECTED_VERSION " || TESTS_RESULT=$?
 }
 
+function assert_get_last_version_tag_from_github {
+  local REPO=$1
+  local EXPECTED_TAG=$2
+  local ACTUAL_TAG=$(get_last_version_tag_from_github "${REPO}")
+  assert_eq "${ACTUAL_TAG}" "${EXPECTED_TAG}" "Last tag of ${REPO} should be ${EXPECTED_TAG} " || TESTS_RESULT=$?
+}
+
 log_header "Tests for get_latest_patch_version"
 assert_latest_patch_version "4.2.1" "4.2.8"
 assert_latest_patch_version "4.2" "4.2.8"
@@ -94,5 +101,10 @@ assert_latest_patch_versions_not_contain "4.2" "3.9.4"
 assert_latest_patch_versions_not_contain "4.2" "4.1.10"
 LATEST_5_4_DEVEL="$(git tag | sort -V | grep 'v5.4.0-DEVEL-' | tail -n 1 | cut -c2-)"
 assert_latest_patch_versions_not_contain "5.3" "$LATEST_5_4_DEVEL"
+
+log_header "Tests for get_last_version_tag_from_github"
+# Use archived repos for tests to ensure constant results
+assert_get_last_version_tag_from_github hazelcast/management-center-openshift v5.3.2
+assert_get_last_version_tag_from_github hazelcast/hazelcast-jet v4.5.4
 
 assert_eq 0 "$TESTS_RESULT" "All tests should pass"
