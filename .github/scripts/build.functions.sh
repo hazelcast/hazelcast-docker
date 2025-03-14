fb8de84cb28f5b194abd2faf6a616d187017eb49
Jack <jack.green@hazelcast.com>
2025-03-14 10:30:46 +0000

    Drop support for running `tag_image_push` _just_ for `README` updates

--- .github/scripts/build.functions.sh
+++ .github/scripts/build.functions.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail ${RUNNER_DEBUG:+-x}
 
-# Checks if we should build the OSS docker image.
+# Checks if we should build the OSS artefacts.
 # Returns "yes" if we should build it or "no" if we shouldn't.
 function should_build_oss() {
 
@@ -14,33 +14,11 @@ function should_build_oss() {
   fi
 }
 
-# Checks if we should build the OSS docker image.
+# Checks if we should build EE artefacts.
 function should_build_ee() {
 
   local release_type=$1
   if [[ $release_type =~ ^(ALL|EE)$ ]]; then
-    echo "yes"
-  else
-    echo "no"
-  fi
-}
-
-# Checks if we should rebuild the docker EE image description.
-function should_build_readme_ee() {
-
-  local release_type=$1
-  if [[ $release_type =~ ^(ALL|EE|README)$ ]]; then
-    echo "yes"
-  else
-    echo "no"
-  fi
-}
-
-# Checks if we should rebuild the docker OSS image description.
-function should_build_readme_oss() {
-
-  local release_type=$1
-  if [[ $release_type =~ ^(ALL|OSS|README)$ ]]; then
     echo "yes"
   else
     echo "no"
--- .github/scripts/build.functions_tests.sh
+++ .github/scripts/build.functions_tests.sh
@@ -28,51 +28,18 @@ function assert_should_build_ee {
   assert_eq "$expected_should_build_ee" "$actual" "$MSG" && log_success "$MSG" || TESTS_RESULT=$?
 }
 
-function assert_should_build_readme_ee {
-  local release_type=$1
-  local expected_should_build_ee=$2
-  local actual=$(should_build_readme_ee "$release_type")
-  local MSG="For release_type=$release_type we should$( [ "$expected_should_build_ee" = "no" ] && echo " NOT") build Readme EE"
-  assert_eq "$expected_should_build_ee" "$actual" "$MSG" && log_success "$MSG" || TESTS_RESULT=$?
-}
-
-function assert_should_build_readme_oss {
-  local release_type=$1
-  local expected_should_build_readme_oss=$2
-  local actual=$(should_build_readme_oss "$release_type")
-  local MSG="For release_type=$release_type we should$( [ "$expected_should_build_readme_oss" = "no" ] && echo " NOT") build Readme OSS"
-  assert_eq "$expected_should_build_readme_oss" "$actual" "$MSG" && log_success "$MSG" || TESTS_RESULT=$?
-}
-
 log_header "Tests for should_build_oss"
 assert_should_build_oss  "ALL" "yes"
 assert_should_build_oss  "OSS" "yes"
 assert_should_build_oss  "EE" "no"
-assert_should_build_oss  "README" "no"
 assert_should_build_oss  "dummy value" "no"
 
 log_header "Tests for should_build_ee"
 assert_should_build_ee  "ALL" "yes"
 assert_should_build_ee  "OSS" "no"
 assert_should_build_ee  "EE" "yes"
-assert_should_build_ee  "README" "no"
 assert_should_build_ee  "dummy value" "no"
 
-log_header "Tests for should_build_readme_oss"
-assert_should_build_readme_oss  "ALL" "yes"
-assert_should_build_readme_oss  "OSS" "yes"
-assert_should_build_readme_oss  "oss" "no"
-assert_should_build_readme_oss  "EE" "no"
-assert_should_build_readme_oss  "README" "yes"
-assert_should_build_readme_oss  "dummy value" "no"
-
-log_header "Tests for should_build_readme_ee"
-assert_should_build_readme_ee  "ALL" "yes"
-assert_should_build_readme_ee  "OSS" "no"
-assert_should_build_readme_ee  "EE" "yes"
-assert_should_build_readme_ee  "ee" "no"
-assert_should_build_readme_ee  "README" "yes"
-assert_should_build_readme_ee  "dummy value" "no"
 
 
 assert_eq 0 "$TESTS_RESULT" "All tests should pass"
