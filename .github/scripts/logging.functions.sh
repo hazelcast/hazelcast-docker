976a4b27b6fe9355119ccbbaf73b51756269adc2
Jack Green <jack.green@hazelcast.com>
2025-05-14 12:22:11 +0100

    Fix RedHat `Image you are trying to publish did not pass the certification test` error [DI-469] [5.3.8] (#977)
    Backports:
    - https://github.com/hazelcast/hazelcast-docker/pull/968
    - https://github.com/hazelcast/hazelcast-docker/pull/845 (partially, to
    address conflicts)

--- .github/scripts/logging.functions.sh
+++ .github/scripts/logging.functions.sh
@@ -8,6 +8,12 @@ function echoerr() {
   echo "::error::ERROR - $*" 1>&2;
 }
 
+# Prints the given message to debug logs, _if enabled_
+function echodebug() {
+  # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#setting-a-debug-message
+  echo "::debug::$*" 1>&2;
+}
+
 # Create group
 function echo_group() {
   # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#grouping-log-lines
