2f51668a73add66941d8cefa9d58ea9b6517d370
Jack Green <jack.green@hazelcast.com>
2025-08-20 15:51:53 +0100

    https://github.com/hazelcast/hazelcast-docker/pull/1053#pullrequestreview-3136998253

--- .github/scripts/publish-rhel.sh
+++ .github/scripts/publish-rhel.sh
@@ -116,12 +116,12 @@ publish_the_image()
         curl --fail \
              --silent \
              --show-error \
-            --retry 5 --retry-all-errors \
-            --header "X-API-KEY: ${RHEL_API_KEY}" \
-            --header 'Cache-Control: no-cache' \
-            --header 'Content-Type: application/json' \
-            --data "{\"image_id\":\"${IMAGE_ID}\" , \"operation\" : \"publish\" }" \
-            "https://catalog.redhat.com/api/containers/v1/projects/certification/id/${RHEL_PROJECT_ID}/requests/images")
+             --retry 5 --retry-all-errors \
+             --header "X-API-KEY: ${RHEL_API_KEY}" \
+             --header 'Cache-Control: no-cache' \
+             --header 'Content-Type: application/json' \
+             --data "{\"image_id\":\"${IMAGE_ID}\" , \"operation\" : \"publish\" }" \
+             "https://catalog.redhat.com/api/containers/v1/projects/certification/id/${RHEL_PROJECT_ID}/requests/images")
 
     echo "Response: ${RESPONSE}"
     echo "Created a publish request, please check if the image is published."
