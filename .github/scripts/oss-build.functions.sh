5c97f0eb4aa65727817009a44c9b1bc4da2fd927
Jack Green <JackPGreen@Gmail.com>
2025-08-29 15:57:38 +0100

    https://github.com/hazelcast/hazelcast-docker/pull/1066#discussion_r2310271594

--- .github/scripts/oss-build.functions.sh
+++ .github/scripts/oss-build.functions.sh
@@ -7,7 +7,7 @@ function get_hz_dist_zip() {
   # The slim is an artifact with a classifier, need to add `-` there
   suffix=${hz_variant:+-$hz_variant}
 
-  if [[ "${hz_version}" == *"SNAPSHOT"* ]]
+  if [[ "${hz_version}" =~ SNAPSHOT ]]
   then
       url="https://${HZ_SNAPSHOT_INTERNAL_USERNAME}:${HZ_SNAPSHOT_INTERNAL_PASSWORD}@repository.hazelcast.com/snapshot-internal/com/hazelcast/hazelcast-distribution/${hz_version}/hazelcast-distribution-${hz_version}${suffix}.zip"
   else
