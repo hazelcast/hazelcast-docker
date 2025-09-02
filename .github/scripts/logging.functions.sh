509ae70d922ab17f86f84acb58fa7cc59c1d6ada
Jack Green <jack.green@hazelcast.com>
2025-09-02 11:30:31 +0100

    https://github.com/hazelcast/hazelcast-docker/pull/1079#pullrequestreview-3175747853

--- .github/scripts/logging.functions.sh
+++ .github/scripts/logging.functions.sh
@@ -3,40 +3,45 @@
 # Prints the given message to stderr
 function echoerr() {
   # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#setting-an-error-message
-  # Support multi-line strings by replacing line separator with GitHub Actions compatible one
-  echo "::error::ERROR - ${*//$'\n'/%0A}" 1>&2;
+  __log "error" "ERROR - " "${*}"
 }
 
 # Prints the given message as a warning
 function echowarning() {
   # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#setting-a-warning-message
-  # Support multi-line strings by replacing line separator with GitHub Actions compatible one
-  echo "::warning::${*//$'\n'/%0A}" 1>&2;
+  __log "warning" "${*}"
 }
 
 # Prints the given message as a notice
 function echonotice() {
   # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#setting-a-notice-message
-  # Support multi-line strings by replacing line separator with GitHub Actions compatible one
+  # __log "notice" "${*}"
   echo "::notice::${*//$'\n'/%0A}" 1>&2;
 }
 
 # Prints the given message to debug logs, _if enabled_
 function echodebug() {
   # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#setting-a-debug-message
-  # Support multi-line strings by replacing line separator with GitHub Actions compatible one
-  echo "::debug::${*//$'\n'/%0A}" 1>&2;
+  __log "debug" "${*}"
 }
 
 # Create group
 function echo_group() {
   # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#grouping-log-lines
-  local TITLE=$1
-  echo "::group::${TITLE}"
+  __log "group" "${*}"
 }
 
 # Ends group after calling echo_group()
 function echo_group_end() {
   # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#grouping-log-lines
-  echo "::endgroup::"
+  __log "endgroup"
+}
+
+function __log() {
+  local level=${1}
+  shift
+  local message=${*}
+
+  # Support multi-line strings by replacing line separator with GitHub Actions compatible one
+  echo "::${level}::${message//$'\n'/%0A}" 1>&2
 }
