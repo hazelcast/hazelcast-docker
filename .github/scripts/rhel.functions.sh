f343d07e5ff9cc8831a8088a8720cb15b9fb47e8
Jack <jack.green@hazelcast.com>
2026-01-12 12:40:24 +0000

    Simplify RedHat publish polling - omit `PROJECT_ID`
    When polling RedHat image publish status, we supply _both_ `IMAGE_ID` and `PROJECT_ID`.
    
    Images IDs are already unique (hence there's an API that queries by `IMAGE_ID` alone), so we can swap to a different endpoint that returns the same result (`ContainerImage`) without needing to pass `PROJECT_ID` along the way.

--- .github/scripts/rhel.functions.sh
+++ .github/scripts/rhel.functions.sh
@@ -3,18 +3,17 @@
 
 get_image()
 {
-  local project_id=$1
-  local image_id=$2
-  local api_key=$3
+  local image_id=$1
+  local api_key=$2
 
   local response
-  # https://catalog.redhat.com/api/containers/docs/endpoints/RESTGetImagesForCertProjectById.html
+  # https://catalog.redhat.com/api/containers/docs/endpoints/RESTGetImage.html
   response=$( \
     curl --fail \
       --silent \
       --show-error \
       --header "X-API-KEY: ${api_key}" \
-      "https://catalog.redhat.com/api/containers/v1/projects/certification/id/${project_id}/images?filter=_id==${image_id}")
+      "https://catalog.redhat.com/api/containers/v1/images/id/${image_id}")
 
   echodebug "${response}"
   echo "${response}"
@@ -24,13 +23,12 @@ get_image()
 # Blocks/waits until the specified image is marked as published
 await_image_publish()
 {
-  local project_id=$1
-  local image_id=$2
-  local api_key=$3
+  local image_id=$1
+  local api_key=$2
 
   while true; do
     local image
-    image=$(get_image "${project_id}" "${image_id}" "${api_key}")
+    image=$(get_image "${image_id}" "${api_key}")
 
     # Check the published status of the most recent repository entry
     if jq --exit-status '.data[].repositories[-1].published' <<< "${image}"; then
