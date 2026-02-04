9c19e42d22ab28728b58e352de1cdaeedbdabf51
Jack Green <jack.green@hazelcast.com>
2026-02-04 20:19:59 +0000

    Check RedHat contains required tags after publish, repair if not [DI-727] (#1218)
    When a new tag is pushed to RedHat - it's not immediately available.
    On the RedHat side a `sync-tags` operation is asynchronously triggered
    to make the new tag accessible.
    
    We have seen repeated instances where this operation fails and the new
    tag is not accessible. As the `push` operation completes successfully,
    we only identify when downstream monitors report the tag is no longer
    accessible. Specifically, we believe there is a race conditions when
    multiple images' tags are being synchronized in a parallel - but
    attempts to workaround this with mutexes have been unsuccessful.
    
    A manual invocation of `sync-tags` addresses this issue.
    
    While we [await the results of RedHat's
    investigation](https://connect.redhat.com/support/partner-acceleration-desk/#/case/04360489),
    we need to do _something_ to maintain the availability of our images -
    as such:
    1. after publish, we poll for the completion of the `sync-tags`
    operation
    2. once completed, we validate the tags on the published image against
    the tags we published - if any are missing, we retrigger a `sync-tags`
    operation
    3. roll back to (1) and try again
    
    This gives two benefits:
    - if a tag is missing, it'll be clear from the job logs (not some
    downstream monitor)
    - it should resolve itself and workaround the issue
    
    [Example (sandbox)
    execution](https://github.com/hazelcast/hazelcast-docker/actions/runs/21529134176/job/62041061755).
    
    Fixes: [DI-727](https://hazelcast.atlassian.net/browse/DI-727)
    
    [DI-727]:
    https://hazelcast.atlassian.net/browse/DI-727?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- .github/scripts/rhel.functions.sh
+++ .github/scripts/rhel.functions.sh
@@ -1,53 +1,150 @@
 # shellcheck source=../.github/scripts/logging.functions.sh
 . .github/scripts/logging.functions.sh
 
-get_certification_project_image_request()
+__get_certification_project_image_request()
+{
+  local image_id=$1
+  local api_key=$2
+  local operation=$3
+
+  # https://catalog.redhat.com/api/containers/docs/endpoints/RESTGetImageRequestsByImageId.html
+  curl --fail \
+    --silent \
+    --show-error \
+    --header "X-API-KEY: ${api_key}" \
+    "https://catalog.redhat.com/api/containers/v1/images/id/${image_id}/requests?filter=operation==${operation}"
+
+  return 0
+}
+
+__get_image_tags()
 {
   local image_id=$1
   local api_key=$2
 
   local response
-  # https://catalog.redhat.com/api/containers/docs/endpoints/RESTGetImageRequestsByImageId.html
+  # https://catalog.redhat.com/api/containers/docs/endpoints/RESTGetImage.html
   response=$( \
     curl --fail \
       --silent \
       --show-error \
       --header "X-API-KEY: ${api_key}" \
-      "https://catalog.redhat.com/api/containers/v1/images/id/${image_id}/requests?filter=operation==publish")
+      "https://catalog.redhat.com/api/containers/v1/images/id/${image_id}")
 
-  echo "${response}"
+  jq --raw-output '[.repositories[].tags[].name] | unique | .[]' <<< "${response}"
   return 0
 }
 
-# Blocks/waits until the specified image is marked as published
-await_image_publish()
+__sync_tags()
+{
+  local project_id=$1
+  local image_id=$2
+  local api_key=$3
+
+  # https://catalog.redhat.com/api/containers/docs/endpoints/RESTPostImageRequestByCertProjectId.html
+  curl --fail \
+        --silent \
+        --show-error \
+        --header "X-API-KEY: ${api_key}" \
+        --header 'Content-Type: application/json' \
+        --data "{\"image_id\":\"${image_id}\" , \"operation\" : \"sync-tags\" }" \
+        "https://catalog.redhat.com/api/containers/v1/projects/certification/id/${project_id}/requests/images"
+
+  return 0
+}
+
+contains_all_expected_tags() {
+  local expected_tags=$1
+  local actual_tags=$2
+
+  for expected_tag in ${expected_tags}
+  do
+    if contains_expected_tag "${expected_tag}" "${actual_tags}"; then
+      echodebug "${expected_tag} found in ${actual_tags}"
+    else
+      return 1
+    fi
+  done
+
+  echo "${expected_tag} not found in ${actual_tags}"
+  return 0
+}
+
+contains_expected_tag() {
+  local expected_tag=$1
+  local actual_tags=$2
+
+  for actual_tag in ${actual_tags}
+  do
+    if [[ "${expected_tag}" == "${actual_tag}" ]]; then
+      echodebug "${expected_tag} found in ${actual_tags}"
+      return 0
+    fi
+  done
+
+  echo "${expected_tag} not found in ${actual_tags}"
+  return 1
+}
+
+# Blocks/waits until the specified image operation is completed
+await_image_operation()
 {
   local image_id=$1
   local api_key=$2
+  local operation=$3
 
   while true; do
     local request
-    request=$(get_certification_project_image_request "${image_id}" "${api_key}")
+    request=$(__get_certification_project_image_request "${image_id}" "${api_key}" "${operation}")
     
     # Print request for status purposes
     jq <<< "${request}"
 
-    # Check the published status of the most recent repository entry
-    case "$(jq -r '.data[-1].status' <<< "${request}")" in
+    local status
+    status=$(jq -r '.data[-1].status' <<< "${request}")
+
+    # Check the status of the most recent repository entry
+    case "${status}" in
       "completed")
-        echo "Image '${image_id}' is published, exiting."
+        echo "Image '${image_id}' ${operation} operation completed"
         return 0
         ;;
       "aborted" | "failed")
-        echo_group "Image '${image_id}' failed to publish"
+        echo_group "Image '${image_id}' failed to complete ${operation}"
         echoerr "$(jq <<< "${request}" || true)"
         echo_group_end
         return 1
         ;;
       *)
-        echo "Image '${image_id}' is still not published, waiting..."
+        echo "Image '${image_id}' still not finished ${operation} (is ${status}), waiting..."
         sleep 5
         ;;
     esac
+  done
+}
+
+# Checks image has expected tags - if not, calls "sync-tags" until it does
+check_image_tags()
+{
+  local project_id=$1
+  local image_id=$2
+  local api_key=$3
+  local expected_tags=$4
+
+  while true; do
+    await_image_operation "${image_id}" "${api_key}" "sync-tags"
+
+    local actual_tags
+    actual_tags=$(__get_image_tags "${image_id}" "${api_key}")
+
+    echodebug "Checking actual tags (${actual_tags}) contains all expected tags (${expected_tags})"
+
+    if contains_all_expected_tags "${expected_tags}" "${actual_tags}"; then
+      return 0
+    else 
+      echo "Resyncing tags"
+      __sync_tags "${project_id}" "${image_id}" "${api_key}"
+    fi
+
   done
 }
