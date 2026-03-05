a770adc666c41e53cf650163ed14cf217ac94fbc
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2026-03-05 21:00:18 +0100

    Fix fake-zip function to work with relative paths

--- .github/scripts/fake-zip.functions.sh
+++ .github/scripts/fake-zip.functions.sh
@@ -5,13 +5,13 @@ create_fake_distribution_zip() {
         return 1
     fi
 
-    local dist_dir
-    dist_dir=$(mktemp -d)
+    local abs_output_zip=$(realpath -m "${output_zip}")
+    local dist_dir=$(mktemp -d)
     mkdir -p "${dist_dir}/hazelcast-0.0.0/bin" "${dist_dir}/hazelcast-0.0.0/lib"
     printf '#!/bin/bash\n' > "${dist_dir}/hazelcast-0.0.0/bin/hz"
     printf '#!/bin/bash\n' > "${dist_dir}/hazelcast-0.0.0/bin/hz-healthcheck"
     chmod +x "${dist_dir}/hazelcast-0.0.0/bin/"*
     touch "${dist_dir}/hazelcast-0.0.0/lib/placeholder"
-    (cd "${dist_dir}" && zip -qr "${output_zip}" hazelcast-0.0.0/)
+    (cd "${dist_dir}" && zip -qr "${abs_output_zip}" hazelcast-0.0.0/)
     rm -rf "${dist_dir}"
 }
