124a15eb48e8edf32357b7050d9f6728bb8bd81e
Jack Green <JackPGreen@Gmail.com>
2026-06-24 17:44:48 +0100

    Ensure `fail` option used with curl to catch errors
    By default, curl returns a `0` exit code even in the case of 404 errors.
    
    This means that errors like the one in https://github.com/hazelcast/hazelcast-mono/pull/7091 are tricky to spot. It would be better if we used the [`fail` option](https://curl.se/docs/manpage.html#--fail) to ensure the failure is visible.

--- .github/scripts/logging.functions.sh
+++ .github/scripts/logging.functions.sh
@@ -1,2 +1,2 @@
 # Delegate to "github-actions-common-scripts" implementation
-source /dev/stdin <<< "$(curl --silent https://raw.githubusercontent.com/hazelcast/github-actions-common-scripts/main/logging.functions.sh)"
+source /dev/stdin <<< "$(curl --fail --silent https://raw.githubusercontent.com/hazelcast/github-actions-common-scripts/main/logging.functions.sh)"
