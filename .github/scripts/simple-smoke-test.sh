790da99857580f446272e0fa516c40abdb9389da
Jack Green <jack.green@hazelcast.com>
2026-08-13 21:03:04 +0100

    In smoke test, merge Docker logs into main job execution logs instead of distinct artifact (#1300)
    When we run the Docker container as part of the smoke test, we run it in
    [detached
    mode](https://docs.docker.com/reference/cli/docker/container/run/#detach)
    to allow subsequent steps to continue. But when running in a detached
    way, logs are not displayed - so we have a separate step to dump and
    upload those logs as an artifact.
    
    GitHub artifacts [consume storage
    credits](https://docs.github.com/en/billing/concepts/product-billing/github-actions#how-storage-billing-works),
    and this artifact exists only as a workaround to capture the logs.
    
    [Using the new `background`
    functionality](https://docs.github.com/en/actions/reference/workflows-and-actions/workflow-syntax#jobsjob_idstepsbackground),
    we can then convert this detached execution into an _attached_ execution
    of a background step - and the logs are then rendered as part of the job
    logs, and no storage credits are consumed:
    <img width="1040" height="872" alt="image"
    src="https://github.com/user-attachments/assets/430e1db7-b10d-422e-80c6-e7dbbe7c6f15"
    />

--- .github/scripts/simple-smoke-test.sh
+++ .github/scripts/simple-smoke-test.sh
@@ -8,28 +8,8 @@ set -o errexit ${RUNNER_DEBUG:+-x}
 # shellcheck source=../.github/scripts/version.functions.sh
 . .github/scripts/version.functions.sh
 
-function remove_container_if_exists() {
-    local containers
-    containers=$(docker ps --all --quiet --filter name="${container_name}")
-
-    if [[ -n "${containers}" ]]; then
-      echo "Removing existing '${container_name}' container"
-      docker container rm --force "${container_name}"
-    fi
-}
-
-function start_container() {
-    echo "Starting container '${container_name}' from image '${image}'"
-    docker run -it --name "${container_name}" -e HZ_LICENSEKEY -e HZ_INSTANCETRACKING_FILENAME -d -p5701:5701 "${image}"
-}
-
 function get_hz_logs() {
     docker logs "${container_name}"
-}
-
-function stop_container() {
-    echo "Stopping container ${container_name}"
-    docker stop "${container_name}"
 }
 
 function check_java_version() {
@@ -78,12 +58,6 @@ container_name=$2
 input_distribution_type=$3
 expected_version=$4
 expected_java_major_version=$5
-
-
-remove_container_if_exists
-start_container
-
-trap stop_container EXIT
 
 expected_distribution_type=$(derive_expected_distribution_type "${input_distribution_type}")
 test_package "${expected_distribution_type}" "${expected_version}"
